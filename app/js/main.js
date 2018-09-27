6f5dcc7b58335c8ecc40000f791745adb9c8ee63
ANDREY_DESIGNER\Andrey <kaoru-san@yandex.ru>
2018-09-27 20:47:17 +0300

    All stuff

---
+++ app/js/main.js
@@ -0,0 +1,41 @@
+// Импортируем jQuery
+//= ../../node_modules/jquery/dist/jquery.js
+
+// POPPER
+//= ../../node_modules/popper.js/dist/umd/popper.js
+
+
+// Импортируем необходимые js-файлы Bootstrap 4
+//= ../../node_modules/bootstrap/js/dist/util.js
+//= ../../node_modules/bootstrap/js/dist/alert.js
+//= ../../node_modules/bootstrap/js/dist/carousel.js
+//= ../../node_modules/bootstrap/js/dist/dropdown.js
+//= ../../node_modules/bootstrap/js/dist/modal.js
+//= ../../node_modules/bootstrap/js/dist/tab.js
+//= ../../node_modules/bootstrap/js/dist/collapse.js
+
+// OWL.CAROUSEL
+//= ../../node_modules/owl.carousel/dist/owl.carousel.min.js
+
+// FONTAWESOME
+//= ../../node_modules/@fortawesome/fontawesome-pro/js/fontawesome.min.js
+//= ../../node_modules/@fortawesome/fontawesome-pro/js/brands.min.js
+//= ../../node_modules/@fortawesome/fontawesome-pro/js/solid.min.js
+
+// BIRMANIZATION
+//= AnchorBirmanizer.js
+
+// DOUBLE HOVER
+//= doubleHover.js
+
+// Импортируем ScrollMagic
+//= ../../node_modules/gsap/umd/TweenMax.js
+//= ../../node_modules/scrollmagic/scrollmagic/uncompressed/ScrollMagic.js
+//= ../../node_modules/scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js
+//= ../../node_modules/scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js
+
+// TYPOGRAF
+//= ../../node_modules/typograf/dist/typograf.min.js
+
+// Импортируем другие js-файлы
+//= app.js
